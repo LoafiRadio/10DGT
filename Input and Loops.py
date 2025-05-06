4ea7d4639e04576e6d247657a302031fcfb38b87
LoafiRadio <samuel.lee2@kingsway.school.nz>
2025-05-07 10:55:11 +1200

    Add files via upload

---
+++ Input and Loops.py
@@ -0,0 +1,20 @@
+#Unput and loops
+#Name of author: Samuel Lee
+#Date: 2025-5-7
+#Version: 1.0.0
+
+#ask the user a question and store the answer in a variable
+#ask the user for their name and store it
+name = input("Kia ora, what is your name? ")
+
+#ask the user for two numbers and add them together
+num1 = int(input("What is your first number? "))
+num2 = int(input("What is your second number? "))
+print(f"You enterned your first number as {num1}")
+print(f"You entered your second number as {num2}")
+sum = int(num1) + int(num2)
+print(f"The sum of {num1} and {num2} is {sum}")
+
+#test that input was stored correctly
+print(f"Hello {name}, welcome!")
+# to comment code out, use cntrl + /
